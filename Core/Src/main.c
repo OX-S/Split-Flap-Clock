c0696e39911541f537621b979195296c068100a9
Tedsen Praktikant1 <praktikant1@TEDNET.local>
2022-07-14 09:44:20 +0200

    added HAL magnet sensing

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -58,6 +58,7 @@ int StepCount;
 time tm;
 int milliseconds;
 int stepFlag = 0;
+GPIO_PinState lastPosition1;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -73,70 +74,11 @@ void step(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
-  milliseconds = 0;
-  tm.seconds = 0;
-  tm.minutes = 0;
-  tm.hours = 0;
-  
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_I2C2_Init();
-  MX_SPI2_Init();
-  /* USER CODE BEGIN 2 */
-  initStep(deg90/10);
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-    if (milliseconds >= 1000) {
-      tm.seconds++;
-      milliseconds = 0;
-    }
-    if (tm.seconds >= 1) {
-      initStep(deg90/10 + 1);
-      tm.seconds = 0;
-    }
-    step();
-    HAL_Delay(1);
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-}
 void initStep(int steps) {
   StepCount = steps;
   flag = 1;
 }
+
 void step(void) {
   if (flag == 1) {
     switch(run) 
@@ -179,6 +121,76 @@ void step(void) {
     }
   }
 }
+
+
+/* USER CODE END 0 */
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+  milliseconds = 0;
+  tm.seconds = 0;
+  tm.minutes = 0;
+  tm.hours = 0;
+  
+  
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C2_Init();
+  MX_SPI2_Init();
+  /* USER CODE BEGIN 2 */
+  initStep(deg90/10);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  lastPosition1 = HAL_GPIO_ReadPin(GPIOC, MAG1_Pin);
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+    if (milliseconds >= 1000) {
+      tm.seconds++;
+      milliseconds = 0;
+    }
+    if (tm.seconds >= 1) {
+      if (flag != 1) {
+        initStep(deg90/10 + 1);
+      }
+      tm.seconds = 0;
+    }
+    step();
+    HAL_Delay(1);
+    if(lastPosition1 != HAL_GPIO_ReadPin(GPIOC, MAG1_Pin)) {
+      initStep(deg90*3);
+      lastPosition1 = HAL_GPIO_ReadPin(GPIOC, MAG1_Pin);
+    }
+  }
+  /* USER CODE END 3 */
+}
+
 /**
   * @brief System Clock Configuration
   * @retval None
